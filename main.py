064ccdacfa3706cd38262b74b72e6cf17aef0855
Abdumajid05 <abdumajidabdulazizov513@gmail.com>
2025-01-25 19:48:31 +0500

    insert database

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from tinydb import TinyDB
 import tinydb
 
-db = TinyDB('db.json')
+db = TinyDB('db.json',indent=4)
+
+data1 = {'first_name': 'Adham', 'last_name': 'Turopov', 'job': 'Software Engineer'}
+data2 = {'first_name': 'Aziz', 'last_name': 'Aliyev', 'job': 'Project Manager'}
+
+db.insert_multiple([data1, data2])
 
