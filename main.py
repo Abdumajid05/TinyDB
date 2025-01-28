22134bd2fe78e468597ac9df32bd86292fd0dd81
Abdumajid05 <abdumajidabdulazizov513@gmail.com>
2025-01-28 16:10:14 +0500

    phone create database

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from tinydb import TinyDB
 import tinydb
 import requests
-db = TinyDB('db.json',indent=4)
+db = TinyDB('phone.json',indent=4)
 
-url = "https://randomuser.me/api/"
-response = requests.get(url)
-data = response.json()
-for user in data['results']:
-    db.insert({'last_name': user['name']['last'], 'first_name': user['name']['first'], 'age': user['dob']['age'], 'gender': user['gender']})  
-users = db.all()
-for user in users:
-    print(user)
-db.remove(tinydb.where('first_name') == 'Aziz')
